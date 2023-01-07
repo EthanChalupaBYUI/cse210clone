db126768b1e8a7732dd383eaf88942a0c2be3dd5
Ethan Chalupa <cha21093@byui.edu>
2023-01-07 14:12:13 -0700

    added prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade?:");
+        string userInput = Console.ReadLine();
+        try
+        {
+            int grade = int.Parse(userInput);
+
+            if (grade >= 90)
+            {
+                Console.WriteLine("You got an A.");
+            }
+            else if (grade >= 80 && grade < 90)
+            {
+                Console.WriteLine("You got an B.");
+            }
+            else if (grade >= 70 && grade < 80)
+            {
+                Console.WriteLine("You got an C.");
+            }
+            else if (grade >= 60 && grade < 70)
+            {
+                Console.WriteLine("You got an D.");
+            }
+            else if (grade < 60)
+            {
+                Console.WriteLine("You got an F, you suck.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input.");
+            }
+        }
+        catch{
+            Console.WriteLine("Nice, you failed to put in a valid grade");
+        }
     }
 }
